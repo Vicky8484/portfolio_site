733aa9c29f691010b1f7dfa465fd104ef8ff2373
“Victory <viok0001@stud.ek.dk>
2026-09-03 21:18:47 +0200

    Add dialog messages to unavailable project links

---
+++ javascript/dialog.js
@@ -0,0 +1,22 @@
+/*dialog boks på links der ikke virker*/
+const secretTags = document.querySelectorAll(".secret-tag");
+secretTags.forEach(function (secretTag) {
+  secretTag.addEventListener("click", function (event) {
+    event.preventDefault();
+    alert("oops, denne side er ikke længere god nok til at vise frem. Sorry!");
+  });
+});
+
+const secretTags2 = document.querySelectorAll(".secret-tag2");
+secretTags2.forEach(function (secretTag2) {
+  secretTag2.addEventListener("click", function (event) {
+    event.preventDefault();
+    alert("Denne side er en hemmelighed nu, desværre!");
+  });
+});
+
+const secretTag3 = document.querySelector("#secret-tag3");
+secretTag3.addEventListener("click", function (event) {
+  event.preventDefault();
+  alert("Top secret gruppe projekt for virksomhed.");
+});
